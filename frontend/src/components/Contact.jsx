068f9223589f0fcfeef823d9977d2df4a7c64b40
MohdOves <mohammadovescontact@gmail.com>
2025-03-17 12:38:01 +0530

    frontend

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -1,5 +1,6 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -7,8 +8,28 @@ const Contact = () => {
   const [phone, setPhone] = useState("");
   const [message, setMessage] = useState("");
 
-  const handleSendMessage = (e) => {
+  const handleSendMessage = async (e) => {
     e.preventDefault();
+    await axios
+      .post(
+        "http://localhost:4000/api/v1/message/send",
+        { name, email, phone, message },
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      )
+      .then((res) => {
+        setName("");
+        setEmail("");
+        setPhone("");
+        setMessage("");
+        toast.success(res.data.message);
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(error.response.data.message);
+      });
   };
 
   return (
@@ -18,13 +39,34 @@ const Contact = () => {
           <form onSubmit={handleSendMessage}>
             <h2>CONTACT US</h2>
             <div>
-              <input type="text" value={name} placeholder="Your Name" onChange={(e) =>setName(e.target.value)}/>
-              <input type="email" value={email} placeholder="Your Email" onChange={(e) =>setEmail(e.target.value)}/>
-              <input type="number" value={[phone]} placeholder="Your Phone Number" onChange={(e) =>setPhone(e.target.value)}/>
-              
+              <input
+                type="text"
+                value={name}
+                placeholder="Your Name"
+                onChange={(e) => setName(e.target.value)}
+              />
+              <input
+                type="email"
+                value={email}
+                placeholder="Your Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="number"
+                value={phone}
+                placeholder="Phone Number"
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </div>
-            <textarea  rows="10" value={message} placeholder="Your Message" onChange={(e) =>setMessage(e.target.value)} />
-              <button className="btn" type="submit">Send Message</button>
+            <textarea
+              rows="10"
+              value={message}
+              placeholder="Message"
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button className="btn" type="submit">
+              Send Message
+            </button>
           </form>
         </div>
       </div>
